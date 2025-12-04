99c8830d216a7e0bb7be8bdc1ffe5e384ec274d2
i.gunawan <i.gunawan@gdn-commerce.com>
2025-12-04 14:10:33 +0700

    QARND-4092 final project.

--- product/src/main/java/com/gdn/marketplace/product/entity/Product.java
+++ product/src/main/java/com/gdn/marketplace/product/entity/Product.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
+@Data
 public class Product {
     @Id
     private String id;
